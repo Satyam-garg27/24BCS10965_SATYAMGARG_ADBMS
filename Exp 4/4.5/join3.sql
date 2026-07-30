2aa06e7c183087e1086b02bdd638da00c13bb60c
Satyam Garg <satyamgarg755@gmail.com>
2026-07-30 15:59:25 +0530

    left exp

---
+++ Exp 4/4.5/join3.sql
@@ -0,0 +1,14 @@
+-- 1.Employee and Manager Names: Display a list of employee names along with their manager's names. Use the 'employees' table provided.
+
+-- 2.Every Possible Combination: Show every possible combination of 'customer_name' from the 'customers' table and 'product_name' from the 'products' table.
+
+SELECT
+    e1.employee_name AS Employee,
+    e2.employee_name AS Manager
+FROM employees AS e1
+JOIN employees AS e2
+ON e1.manager_id = e2.employee_id;
+
+
+select c.customer_name,p.product_name from customers as c
+cross join products as p;
